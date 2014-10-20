26f06fc8d5db2e8b7e0f298befc41ac0f599063a
Robotpants22 <gto22@cox.net>
2014-10-20 15:31:49 -0700

    Automatically Committed for MakeGamesWithUs

--- FlappyBird.spritebuilder/Source/GamePlayScene.h
+++ FlappyBird.spritebuilder/Source/GamePlayScene.h
@@ -21,7 +21,8 @@ typedef NS_ENUM(NSInteger, DrawingOrder) {
     Character*     character;
     CCPhysicsNode* physicsNode;}
 
--(void) initialize;[self addObstacle];-(void) addObstacle;
+-(void) initialize;
+-(void) addObstacle;
 -(void) showScore;
 
 @end
