cb16e9356695264c039273184dc16f04fe20e3cb
Robotpants22 <gto22@cox.net>
2014-10-20 16:01:24 -0700

    Automatically Committed for MakeGamesWithUs

--- FlappyBird.spritebuilder/Source/GamePlayScene.m
+++ FlappyBird.spritebuilder/Source/GamePlayScene.m
@@ -16,6 +16,6 @@
 // put new methods here
 - (void)touchBegan:(UITouch *)touch withEvent:(UIEvent *)event {
     // this will get called every time the player touches the screen
-    [character flap];}
+    -(type I return)nameOfMethod: (type of first parameter) firstParameter con    [character flap];}
 
 @end
