bc00fb8387f344d76c225d31819e867d86c999af
Robotpants22 <gto22@cox.net>
2014-10-20 17:20:56 -0700

    Automatically Committed for MakeGamesWithUs

--- FlappyBird.spritebuilder/Source/GamePlayScene.m
+++ FlappyBird.spritebuilder/Source/GamePlayScene.m
@@ -6,7 +6,18 @@
 - (void)initialize{
     // your code here-
     [self addObstacle];timeSinceObstacle = 0.0f;
-    character = (Character*)[CCBReader load:@"Character"];
+    // Increment the time since the last obstacle was added
+    timeSinceObstacle += delta; // delta is approximately 1/60th of a second
+    
+    // Check to see if two seconds have passed
+    if (timeSinceObstacle > 2.0f)
+    {
+        // Add a new obstacle
+        [self addObstacle];
+        
+        // Then reset the timer.
+        timeSinceObstacle = 0.0f;
+    }    character = (Character*)[CCBReader load:@"Character"];
     [physicsNode addChild:character];}
 
 -(void)update:(CCTime)delta
